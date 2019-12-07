a6396a54425651056de7c78491bf368d4ebf202f
Basstorm <fuli114.ml@gmail.com>
2019-12-07 15:12:16 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# OpenWrt-K2P
+ 采用P3TERX方案，定制加入Derry's OAF插件以及luci-theme-rosy。
+
+ ## Acknowledgments
+
+- [P3TERX's Actions-OpenWrt](https://github.com/P3TERX/Actions-OpenWrt)
+- [Derry's OAF Plugin](https://github.com/destan19/OpenAppFilter)
+- [luci-theme-rosy](https://github.com/rosywrt/luci-theme-rosy)
---
+++ diy.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+#=================================================
+#   Description: DIY script
+#   Lisence: MIT
+#   Author: P3TERX
+#   Blog: https://p3terx.com
+#=================================================
+git clone https://github.com/destan19/OpenAppFilter openwrt/package/OpenAppFilter
+git clone https://github.com/rosywrt/luci-theme-rosy openwrt/package/luci-theme-rosy
